9bbd90ac053de9ab491fd58164f8cb55c4dabc37
Oscar <110410670+Osc44r@users.noreply.github.com>
2022-09-07 20:55:29 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,134 @@
+if __name__ == '__main__':
+    import sys
+
+    import undetected_chromedriver as uc
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.by import By
+
+    options = Options()
+    options.add_argument("--headless")
+    driver = uc.Chrome(options=options)
+    driver.get("https://backpack.tf/spreadsheet")
+    minref = float(input("Input minimum price in refs to fetch data (min: 0.5, default: 1): "))
+    if minref < 0.5:
+        print("Minimum price can not be lower than 0.5")
+        sys.exit()
+    maxref = float(input("Input maximum price in refs to fetch data (min: 0.5, default: 40): "))
+    if maxref < 0.5:
+        print("Maximum price can not be lower than 0.5")  
+        sys.exit()
+        
+    parseGenuine = input("Parse genuine items? (y/n): ")
+    if parseGenuine != "y" and parseGenuine != "n":
+        print("You were supposed to pick y/n there.")
+        sys.exit()
+    parseVintage = input("Parse vintage items? (y/n): ")
+    if parseVintage != "y" and parseVintage != "n":
+        print("You were supposed to pick y/n there.")
+        sys.exit()
+    parseUnique = input("Parse unique items? (y/n): ")
+    if parseUnique != "y" and parseUnique != "n":
+        print("You were supposed to pick y/n there.")
+        sys.exit()
+    parseStrange = input("Parse strange items? (y/n): ")
+    if parseStrange != "y" and parseStrange != "n":
+        print("You were supposed to pick y/n there.")
+        sys.exit()
+    parseHaunted = input("Parse haunted items? (y/n): ")
+    if parseHaunted != "y" and parseHaunted != "n":
+        print("You were supposed to pick y/n there.")
+        sys.exit()
+    parseCollector = input("Parse collector's items? (y/n): ")
+    if parseCollector != "y" and parseCollector != "n":
+        print("You were supposed to pick y/n there.")
+        sys.exit()
+
+
+    panel = driver.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody")
+    id = 0
+    f = open('./ItemList.txt','w',encoding="utf-8")
+    f.write("Items = [")
+    f.close()
+    for tr in panel.find_elements(By.TAG_NAME,"tr"):
+        id = id+1
+
+        itemname = panel.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody/tr["+str(id)+"]/td[1]")
+        typee = panel.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody/tr["+str(id)+"]/td[2]")
+        genuine = panel.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody/tr["+str(id)+"]/td[3]")
+        vintage = panel.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody/tr["+str(id)+"]/td[4]")
+        unique = panel.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody/tr["+str(id)+"]/td[5]")
+        strange = panel.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody/tr["+str(id)+"]/td[6]")
+        haunted = panel.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody/tr["+str(id)+"]/td[7]")
+        collector = panel.find_element(By.XPATH,"/html/body/main/div[2]/div/div/div[2]/div/table/tbody/tr["+str(id)+"]/td[8]")
+
+        if "Crate" not in itemname.text and "(Non-Craftable)" not in itemname.text and "Tool" not in typee.text:
+            if parseGenuine == "y":
+                if float(genuine.get_attribute("abbr")) > minref and float(genuine.get_attribute("abbr")) < maxref:
+                    print('Genuine "'+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('Genuine "'+itemname.text+'",')
+                    f.close()
+                elif float(genuine.get_attribute("abbr")) != 0 and float(genuine.get_attribute("abbr")) < minref and float(genuine.get_attribute("abbr")) < maxref and "Primary" in typee.text:
+                    print('"Festivized Geniune '+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('"Festivized Genuine '+itemname.text+'",')
+                    f.close()
+            if parseVintage == "y":
+                if float(vintage.get_attribute("abbr")) > minref and float(vintage.get_attribute("abbr")) < maxref:
+                    print('Vintage "'+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('Vintage "'+itemname.text+'",')
+                    f.close()
+                elif float(vintage.get_attribute("abbr")) != 0 and float(vintage.get_attribute("abbr")) < minref and float(vintage.get_attribute("abbr")) < maxref and "Primary" in typee.text:
+                    print('"Festivized Vintage '+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('"Festivized Vintage '+itemname.text+'",')
+                    f.close()
+            if parseUnique == "y":
+                if float(unique.get_attribute("abbr")) > minref and float(unique.get_attribute("abbr")) < maxref:
+                    print('"'+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('"'+itemname.text+'",')
+                    f.close()
+                elif float(unique.get_attribute("abbr")) != 0 and float(unique.get_attribute("abbr")) < minref and float(unique.get_attribute("abbr")) < maxref and "Primary" in typee.text:
+                    print('"Festivized '+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('"Festivized '+itemname.text+'",')
+                    f.close()
+            if parseStrange == "y":
+                if float(strange.get_attribute("abbr")) > minref and float(strange.get_attribute("abbr")) < maxref:
+                    print('"Strange '+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('"Strange '+itemname.text+'",')
+                    f.close()
+                elif float(strange.get_attribute("abbr")) != 0 and float(strange.get_attribute("abbr")) < minref and float(strange.get_attribute("abbr")) < maxref and "Primary" in typee.text:
+                    print('"Strange Festivized '+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('"Strange Festivized '+itemname.text+'",')
+                    f.close()
+            if parseHaunted == "y":
+                if float(haunted.get_attribute("abbr")) > minref and float(haunted.get_attribute("abbr")) < maxref:
+                    print('Haunted "'+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('Haunted "'+itemname.text+'",')
+                    f.close()
+                elif float(haunted.get_attribute("abbr")) != 0 and float(haunted.get_attribute("abbr")) < minref and float(haunted.get_attribute("abbr")) < maxref and "Primary" in typee.text:
+                    print('"Festivized Haunted '+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('"Festivized Haunted '+itemname.text+'",')
+                    f.close()
+            if parseCollector == "y":
+                if float(collector.get_attribute("abbr")) > minref and float(collector.get_attribute("abbr")) < maxref:
+                    print('Collector'"'"'s "'+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('Collector'"'"'s "'+itemname.text+'",')
+                    f.close()
+                elif float(collector.get_attribute("abbr")) != 0 and float(collector.get_attribute("abbr")) < minref and float(haunted.get_attribute("abbr")) < maxref and "Primary" in typee.text:
+                    print('"Festivized Collector'"'"'s '+itemname.text+'"')
+                    f = open('./ItemList.txt','a',encoding="utf-8")
+                    f.write('"Festivized Collector'"'"'s '+itemname.text+'",')
+                    f.close()
+    f = open('./ItemList.txt','a',encoding="utf-8")
+    f.write("]")
+    f.close()
+
